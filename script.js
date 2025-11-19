1c10a0600a8b4a45eab535bc534b6e73b301ca8c
JB_Ash <jashadu123@gmail.com>
2025-11-19 20:04:04 +0000

    Complete equals for touchButton function

--- script.js
+++ script.js
@@ -16,12 +16,19 @@ const touchButton = (e) => {
     else if(e.target.value === "DE") {
         output.value = output.value.slice(0, -1)
     }
-    else if(e.target.value === "π") {}
-    else if(e.target.value === "÷") {}
-    else if(e.target.value === "×") {}
-    else if(e.target.value === "-") {}
-    else if(e.target.value === "+") {}
-    else if(e.target.value === "=") {}
+    else if(e.target.value === "π") {
+        if(output.value === '') {
+            output.value = pi * 1
+        }
+        else{
+            output.value *= pi
+        }
+    }
+    else if(e.target.value === "=") {
+        output.value = output.value.replace("×", "*")
+        output.value = output.value.replace("÷", "/")
+        output.value = eval(output.value)
+    }
     else{
         output.value += e.target.value
     }
