a7152d50928ab248e71582898ad9b856260398ec
JB_Ash <jashadu123@gmail.com>
2025-11-19 19:30:00 +0000

    Create touchButton function and pi const

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const output = document.getElementById("output")
 const inputs = document.getElementsByTagName("input")
 const buttons = []
+const pi = 3.14159265
 
 Array.from(inputs).forEach((input) => {
     if(input.type === "button") {
         buttons.push(input)
     }
 })
+
+const touchButton = (e) => {
+    if(e.target.value === "C") {
+        output.value = ''
+    }
+    else if(e.target.value === "DE") {
+        output.value = output.value.slice(0, -1)
+    }
+    else if(e.target.value === "π") {}
+    else if(e.target.value === "÷") {}
+    else if(e.target.value === "×") {}
+    else if(e.target.value === "-") {}
+    else if(e.target.value === "+") {}
+    else if(e.target.value === "=") {}
+    else{
+        output.value += e.target.value
+    }
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", touchButton)
+}
