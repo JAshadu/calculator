c34ff29698e64c0e7da3800a04dd2e883b7b709a
JB_Ash <jashadu123@gmail.com>
2025-12-27 01:42:53 +0000

    Add afterEquals feature with symbols array

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const output = document.getElementById("output")
 const inputs = document.getElementsByTagName("input")
 const buttons = []
 const pi = 3.14159265
+let afterEquals =  false
+const symbols = ["=", "+", "-", "×", "÷", "C", "DE"]
 
 Array.from(inputs).forEach((input) => {
     if(input.type === "button") {
@@ -10,11 +12,17 @@ Array.from(inputs).forEach((input) => {
 })
 
 const touchButton = (e) => {
+    if(afterEquals && !symbols.includes(e.target.value)) {
+        output.value = ''
+    }
+
     if(e.target.value === "C") {
         output.value = ''
+        afterEquals = false
     }
     else if(e.target.value === "DE") {
         output.value = output.value.slice(0, -1)
+        afterEquals = false
     }
     else if(e.target.value === "π") {
         if(output.value === '') {
@@ -26,14 +34,18 @@ const touchButton = (e) => {
         else{
             output.value *= pi
         }
+        afterEquals = false
     }
     else if(e.target.value === "=") {
+        if(output.value === '') {return}
         output.value = output.value.replace("×", "*")
         output.value = output.value.replace("÷", "/")
         output.value = eval(output.value)
+        afterEquals = true
     }
     else{
         output.value += e.target.value
+        afterEquals = false
     }
 }
 
